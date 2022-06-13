b6086ddc8a5cb44c4d28c7e9e53d22d09f24a622
BrynzevDmitrii <Elf_happy@mail.ru>
2022-06-13 14:55:04 +0300

    Tests

--- src/todo/TodoList.jsx
+++ src/todo/TodoList.jsx
@@ -1,19 +1,35 @@
-import styles from './TodoList.module.css'
-const TodoList = ({item , toggleTask , removeTask}) => {
-    return (  
-    <div key={item.id} className = {styles['wrapper']}>
-        
-        <button
-        onClick={()=>{toggleTask(item.id)}}
-            className = {styles['performed']}>
-            Выполнена</button>
-        <div  className = {item.isCheked?styles['todo-line'] : styles['todo']} >{item.task}</div>
-        <button 
-        onClick={()=>{removeTask(item.id)}}
-        className = {styles['closed']}
-        >&times;</button>
-    </div>
-    );
-}
- 
+import styles from "./TodoList.module.css";
+const TodoList = ({ item, toggleTask, removeTask }) => {
+  return (
+    <ul>
+      <li>
+        <div key={item.id} className={styles["wrapper"]}>
+          <button
+            onClick={() => {
+              toggleTask(item.id);
+            }}
+            className={styles["performed"]}
+          >
+            Выполнена
+          </button>
+          <div
+            data-testid="task"
+            className={item.isCheked ? styles["todo-line"] : styles["todo"]}
+          >
+            {item.task}
+          </div>
+          <button
+            onClick={() => {
+              removeTask(item.id);
+            }}
+            className={styles["closed"]}
+          >
+            &times;
+          </button>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
 export default TodoList;
